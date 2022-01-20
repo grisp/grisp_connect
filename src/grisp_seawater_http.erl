5415093d1731f8d3c2686f000c962a175a162962
GalaxyGorilla <skattelmann@gmail.com>
2022-01-20 17:59:11 +0100

    Refine certificate handling

--- src/grisp_seawater_http.erl
+++ src/grisp_seawater_http.erl
@@ -24,18 +24,15 @@
 open() -> open("seawater.stritzinger.com", 443).
 
 open(ServerName, Port) ->
-    case ssl_opts(ServerName) of
+    GunOpts = #{transport => tls,
+                transport_opts => ssl_opts(ServerName)},
+    case gun:open(ServerName, Port, GunOpts) of
         {error, _Reason} = Error -> Error;
-        {ok, TransOpts} ->
-            GunOpts = #{transport => tls, transport_opts => TransOpts},
-            case gun:open(ServerName, Port, GunOpts) of
+        {ok, Conn} ->
+            erlang:link(Conn),
+            case gun:await_up(Conn) of
                 {error, _Reason} = Error -> Error;
-                {ok, Conn} ->
-                    erlang:link(Conn),
-                    case gun:await_up(Conn) of
-                        {error, _Reason} = Error -> Error;
-                        {ok, _} -> {ok, #?MODULE{conn = Conn}}
-                    end
+                {ok, _} -> {ok, #?MODULE{conn = Conn}}
             end
     end.
 
@@ -61,59 +58,22 @@ get(#?MODULE{conn = Conn}, Path) ->
 %--- Internal Functions --------------------------------------------------------
 
 ssl_opts(ServerName) ->
-    case client_chain() of
-        {error, _Reason} = Error -> Error;
-        {ok, ClientChain} ->
-            case server_chain(ServerName) of
-                {error, _Reason} = Error -> Error;
-                {ok, ServerChain} ->
-                    {ok, [
-                        {server_name_indication, ServerName},
-                        {verify, verify_peer},
-                        {cacerts, ServerChain},
-                        {cert, ClientChain},
-                        {key, #{
-                            algorithm => ecdsa,
-                            sign_fun => {grisp_cryptoauth, sign_fun}
-                        }}
-                    ]}
-            end
-    end.
-
-server_chain(ServerName) ->
-    load_cert_chain(["server", ServerName]).
-
-client_chain() ->
     Templates = [{{2, 0}, {grisp_seawater_cert, grisp2_device}}],
-    ClientCert = grisp_cryptoauth:read_cert(primary, Templates, der),
-    {ok, IssuerId} = public_key:pkix_issuer_id(ClientCert, self),
-    case client_chain_issuer(IssuerId) of
-        {error, _Reason} = Error -> Error;
-        {ok, Chain} -> {ok, [ClientCert | Chain]}
-    end.
+    Cert = grisp_cryptoauth:read_cert(primary, Templates, der),
+    [
+        {server_name_indication, ServerName},
+        {verify, verify_peer},
+        {cacerts, ca_certs()},
+        {cert, Cert},
+        {key, #{
+            algorithm => ecdsa,
+            sign_fun  => {grisp_cryptoauth, sign_fun}
+        }}
+    ].
 
-client_chain_issuer({Serial, _}) when Serial >= 1000 ->
-    load_cert_chain(["client", "batch_001"]).
-
-load_cert_chain(RelPath) ->
-    FilePath = cert_path(RelPath),
-    case file:read_file(FilePath) of
-        {error, enoent} ->
-            {error, {file_not_found, FilePath}};
-        {ok, PemData} ->
-            decode_cert_chain(PemData)
-    end.
-
-cert_path(RelPath) ->
-    PrivDir = code:priv_dir(grisp_seawater),
-    filename:join([PrivDir | RelPath]) ++ ".pem".
-
-decode_cert_chain(PemData) ->
-    decode_cert_chain(public_key:pem_decode(PemData), []).
-
-decode_cert_chain([], Acc) ->
-    {ok, lists:reverse(Acc)};
-decode_cert_chain([{'Certificate', D, not_encrypted} | Rest], Acc) ->
-    decode_cert_chain(Rest, [D | Acc]);
-decode_cert_chain([Bad | _Rest], _Acc) ->
-    {error, {bad_certificate, Bad}}.
+ca_certs() ->
+    Priv = code:priv_dir(grisp_seawater),
+    StritzingerRoot = grisp_cryptoauth_cert:decode_pem_file(filename:join([Priv, "client", "stritzinger_root.pem"]), der),
+    GRiSP2CA = grisp_cryptoauth_cert:decode_pem_file(filename:join([Priv, "client", "grisp2_ca.pem"]), der),
+    SeawaterCA = grisp_cryptoauth_cert:decode_pem_file(filename:join([Priv, "server", "www.seawater.local.pem"]), der),
+    [StritzingerRoot, GRiSP2CA, SeawaterCA].
