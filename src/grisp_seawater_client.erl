6940f4e46735935bee13a093332c3a8f1dc7610f
Luca Succi <luca.succi@stritzinger.com>
2023-02-28 09:34:24 +0100

    First device-api WS with flash feature (#2)
    * First grisp seawater client
    
    * Pack jsonrpc as single when list contains only one message
    
    * Cleanups
    
    * Eunit tests for jsonrpc
    
    * Move jsonrpc unit tests under CT
    
    * Check json pairs in ct
    
    * Flash on async process
    
    * Add github CI

---
+++ src/grisp_seawater_client.erl
@@ -0,0 +1,117 @@
+-module(grisp_seawater_client).
+
+-export([start_link/0]).
+-export([connect/0]).
+-export([connect/2]).
+
+-behaviour(gen_server).
+
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+
+-record(state, {
+    http_conn,
+    ws_stream
+}).
+
+
+-include_lib("kernel/include/logger.hrl").
+
+%--- API Functions -------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+connect() ->
+    connect("seawater.stritzinger.com", 443).
+
+connect(Server, Port) ->
+    gen_server:call(?MODULE, {?FUNCTION_NAME, Server, Port}).
+
+
+% gen_server callbacks -----------------------------
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_call({connect, Server, Port}, _, #state{http_conn = undefined} = S) ->
+    case grisp_seawater_http:open(Server, Port) of
+        {ok, Conn} ->
+            case upgrade(Conn) of
+                {ok, WsStream} ->
+                    {reply, ok, #state{http_conn = Conn, ws_stream = WsStream}};
+                Error ->
+                    ok = grisp_seawater_http:close(Conn),
+                    {reply, Error, S}
+            end;
+        Error ->
+            {reply, Error, S}
+    end;
+handle_call({connect, _Server, _Port}, _,  S) ->
+    {reply, already_connected, S}.
+
+handle_cast(connect, S) ->
+    {noreply, S}.
+
+handle_info({gun_ws, Conn, Stream, {text, JSON}},
+            #state{http_conn = Conn, ws_stream = Stream}= S) ->
+    JSON_RPC = grisp_seawater_jsonrpc:decode(JSON),
+    case handle_jsonrpc(JSON_RPC) of
+        [] -> ok;
+        Msgs ->
+            Text = grisp_seawater_jsonrpc:encode(Msgs),
+            gun:ws_send(S#state.http_conn, S#state.ws_stream, {text, Text})
+    end,
+    {noreply, S};
+handle_info(M, S) ->
+    ?LOG_WARNING("Unandled WS message: ~p", [M]),
+    {noreply, S}.
+
+% internal functions -----------------------------
+
+upgrade(Conn) ->
+    StreamRef = gun:ws_upgrade(Conn, "/grisp-connect/ws"),
+    receive
+    {gun_upgrade, Conn, StreamRef, [<<"websocket">>], _Headers} ->
+        {ok, StreamRef};
+    {gun_response, Conn, _, _, Status, Headers} ->
+        {ws_upgrade_failed, Status, Headers};
+    {gun_error, Conn, StreamRef, Reason} ->
+        {ws_upgrade_failed, Reason}
+    after 1000 ->
+        timeout
+    end.
+
+handle_jsonrpc({batch, Batch}) ->
+    handle_rpc_messages(Batch, []);
+handle_jsonrpc({single, Rpc}) ->
+    handle_rpc_messages([Rpc], []).
+
+handle_rpc_messages([], Replies) -> lists:reverse(Replies);
+handle_rpc_messages([{request, M, Params, ID} | Batch], Replies)
+        when M == <<"post">> ->
+    handle_rpc_messages(Batch, [handle_request(M, Params, ID) | Replies]);
+handle_rpc_messages([{error, _Code, _Msg, _Data, _ID} = E | Batch], Replies) ->
+    ?LOG_DEBUG("Received JsonRPC error: ~p",[E]),
+    handle_rpc_messages(Batch, Replies);
+handle_rpc_messages([{internal_error, _, _} = E | Batch], Replies) ->
+    ?LOG_ERROR("JsonRPC: ~p",[E]),
+    handle_rpc_messages(Batch, [grisp_seawater_jsonrpc:format_error(E)| Replies]).
+
+handle_request(<<"post">>, #{type := <<"flash">>} = Params, ID) ->
+    Led = maps:get(led, Params, 1),
+    Color = maps:get(color, Params, red),
+    {result, flash(Led, Color), ID};
+handle_request(_, _, ID) ->
+    grisp_seawater_jsonrpc:format_error({internal_error, method_not_found, ID}).
+
+flash(Led, Color) ->
+    spawn(fun() ->
+        io:format("Flash from Seawater!~n"),
+        grisp_led:color(Led, Color),
+        timer:sleep(100),
+        grisp_led:off(Led)
+    end),
+    ok.
---
+++ test/jsonrpc_SUITE.erl
@@ -0,0 +1,97 @@
+-module(jsonrpc_SUITE).
+
+-include_lib("stdlib/include/assert.hrl").
+
+-export([all/0]).
+
+-export([positional_parameters/1,
+        named_parameters/1,
+        notification/1,
+        invalid_json/1,
+        invalid_request/1,
+        batch/1,
+        result/1]).
+
+all() -> [
+    positional_parameters,
+    named_parameters,
+    notification,
+    invalid_json,
+    invalid_request,
+    batch,
+    result
+].
+
+positional_parameters(_) ->
+    Term = {request, <<"subtract">>, [42,23], 1},
+    Json = <<"{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"subtract\",\"params\":[42,23]}">>,
+    ?assertMatch({single, Term}, grisp_seawater_jsonrpc:decode(Json)),
+    Json = grisp_seawater_jsonrpc:encode(Term),
+    ?assert(jsonrpc_check([<<"\"id\":1">>,
+                           <<"\"method\":\"subtract\"">>,
+                           <<"\"params\":[42,23]">>], Json)).
+
+named_parameters(_) ->
+    Term = {request, <<"subtract">>, #{<<"subtrahend">> => 23, <<"minuend">> => 42}, 2},
+    Json = <<"{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"subtract\",\"params\":{\"minuend\":42,\"subtrahend\":23}}">>,
+    ?assertMatch({single, Term}, grisp_seawater_jsonrpc:decode(Json)),
+    Json = grisp_seawater_jsonrpc:encode(Term),
+    ?assert(jsonrpc_check([<<"\"id\":2">>,
+                           <<"\"method\":\"subtract\"">>,
+                           <<"\"minuend\":42">>,
+                           <<"\"subtrahend\":23">>], Json)).
+
+notification(_) ->
+    Term = {notification, <<"update">>, [1,2,3,4,5]},
+    Json = <<"{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"params\":[1,2,3,4,5]}">>,
+    ?assertMatch({single, Term}, grisp_seawater_jsonrpc:decode(Json)),
+    Json = grisp_seawater_jsonrpc:encode(Term),
+    ?assert(jsonrpc_check([<<"\"method\":\"update\"">>,
+                           <<"\"params\":[1,2,3,4,5]">>], Json)).
+
+invalid_json(_) ->
+    Term = {internal_error, parse_error, null},
+    Json = <<"{\"jsonrpc\":\"2.0\",\"method\":\"foobar,\"params\":\"bar\",\"baz]">>,
+    ?assertMatch({single, Term}, grisp_seawater_jsonrpc:decode(Json)),
+    JsonError = grisp_seawater_jsonrpc:encode(grisp_seawater_jsonrpc:format_error(Term)),
+    ?assert(jsonrpc_check([<<"\"error\":{">>,
+                            <<"\"code\":-32700">>,
+                            <<"\"message\":\"Parse error\"">>,
+                            <<"\"id\":null">>], JsonError)).
+
+invalid_request(_) ->
+    Term = {internal_error, invalid_request, null},
+    Json = <<"{\"jsonrpc\":\"2.0\",\"method\":1,\"params\":\"bar\"}">>,
+    ?assertMatch({single, Term}, grisp_seawater_jsonrpc:decode(Json)),
+    JsonError = grisp_seawater_jsonrpc:encode(grisp_seawater_jsonrpc:format_error(Term)),
+    ?assert(jsonrpc_check([<<"\"error\":{">>,
+                           <<"\"code\":-32600">>,
+                           <<"\"message\":\"Invalid request\"">>,
+                           <<"\"id\":null">>], JsonError)).
+
+batch(_) ->
+    Term1 = {request, <<"sum">>, [1,2,4], <<"1">>},
+    Term2 = {internal_error, invalid_request, null},
+    Json = <<"[{\"jsonrpc\":\"2.0\",\"method\":\"sum\",\"params\":[1,2,4],\"id\":\"1\"},{\"foo\":\"boo\"}]">>,
+    ?assertMatch({batch, [Term1,Term2]}, grisp_seawater_jsonrpc:decode(Json)),
+    JsonError = grisp_seawater_jsonrpc:encode([Term1, grisp_seawater_jsonrpc:format_error(Term2)]),
+    ?assert(jsonrpc_check([<<"\"id\":\"1\"">>,
+                           <<"\"method\":\"sum\"">>,
+                           <<"params\":[1,2,4]">>,
+                           <<"\"error\":{">>,
+                           <<"\"code\":-32600">>,
+                           <<"\"message\":\"Invalid request\"">>,
+                           <<"\"id\":null">>], JsonError)).
+
+result(_) ->
+    Term = {result, 7, 45},
+    Json = <<"{\"id\":45,\"jsonrpc\":\"2.0\",\"result\":7}">>,
+    ?assertMatch({single, Term}, grisp_seawater_jsonrpc:decode(Json)),
+    Json = grisp_seawater_jsonrpc:encode(Term),
+    ?assert(jsonrpc_check([<<"\"id\":45">>,
+                           <<"\"result\":7">>], Json)).
+
+
+jsonrpc_check(Elements, JsonString) ->
+    Elements2 = [<<"\"jsonrpc\":\"2.0\"">>| Elements],
+    lists:all(fun(E) -> binary:match(JsonString, E) =/= nomatch end, Elements2).
