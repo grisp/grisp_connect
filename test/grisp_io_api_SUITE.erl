2d6f76d6eb74622842566a94b69afa55be2bfaa0
Luca Succi <luca.succi@stritzinger.com>
2024-01-31 17:39:41 +0100

    Add test suite for grisp_io API

---
+++ test/grisp_io_api_SUITE.erl
@@ -0,0 +1,76 @@
+-module(grisp_io_api_SUITE).
+
+-behaviour(ct_suite).
+-include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+-compile([export_all, nowarn_export_all]).
+
+%--- API -----------------------------------------------------------------------
+
+all() ->
+    [
+        F
+        ||
+        {F, 1} <- ?MODULE:module_info(exports),
+        lists:suffix("_test", atom_to_list(F))
+    ].
+
+init_per_suite(Config) ->
+    PrivDir = ?config(priv_dir, Config),
+    DataDir = ?config(data_dir, Config),
+    CertDir = grisp_io_test_util:get_cert_dir(DataDir),
+
+    PolicyFile = filename:join(PrivDir, "policies.term"),
+    ?assertEqual(ok, file:write_file(PolicyFile, <<>>)),
+    application:set_env(seabac, policy_file, PolicyFile),
+
+    Apps = grisp_io_test_util:start_apps(PrivDir, CertDir),
+    [{cert_dir, CertDir} | [{apps, Apps} | Config]].
+
+end_per_suite(Config) ->
+    grisp_io_test_util:cleanup_apps(?config(apps, Config)).
+
+init_per_testcase(_, Config) ->
+    {ok, _} = application:ensure_all_started(grisp_emulation),
+    application:set_env(grisp_io, test_cert_dir, ?config(cert_dir, Config)),
+    {ok, _} = application:ensure_all_started(grisp_io),
+    Config.
+
+end_per_testcase(_, Config) ->
+    ok = application:stop(grisp_io),
+    mnesia:activity(transaction, fun() ->
+        mnesia:delete({grisp_device, <<"0000">>})
+    end),
+    flush(),
+    Config.
+
+%--- Tests ---------------------------------------------------------------------
+
+auto_connect_test(_) ->
+    assert_connection().
+
+ping_test(_) ->
+    assert_connection(),
+    ?assertMatch({ok, <<"pang">>}, grisp_io:ping()).
+
+link_device_test(_) ->
+    assert_connection(),
+    ?assertMatch({error, token_undefined}, grisp_io:link_device()),
+    application:set_env(grisp_io, device_linking_token, <<"token">>),
+    ?assertMatch({error, invalid_token}, grisp_io:link_device()),
+    Token = grisp_manager_token:get_token(<<"Testuser">>),
+    application:set_env(grisp_io, device_linking_token, Token),
+    ?assertMatch({ok, <<"ok">>}, grisp_io:link_device()),
+    ?assertMatch({ok, <<"pong">>}, grisp_io:ping()).
+
+%--- Internal ------------------------------------------------------------------
+
+assert_connection() ->
+    ct:sleep(1000),
+    ?assert(grisp_io:is_connected()).
+
+flush() ->
+    receive Any -> ct:pal("Flushed: ~p", [Any]), flush()
+    after 0 -> ok
+    end.
---
+++ test/grisp_io_test_util.erl
@@ -0,0 +1,74 @@
+-module(grisp_io_test_util).
+
+-compile([export_all, nowarn_export_all]).
+
+start_apps(PrivDir, CertDir) ->
+    application:set_env(mnesia, dir, PrivDir),
+
+    eresu:install([node()]),
+    {ok, Started1} = application:ensure_all_started(eresu),
+    register_user(),
+
+    grisp_manager:install([node()]),
+
+    application:start(mnesia),
+
+    {ok, Started2} = application:ensure_all_started(kraft),
+    SslOpts = [
+        {verify, verify_peer},
+        {keyfile, filename:join(CertDir, "server.key")},
+        {certfile, filename:join(CertDir, "server.crt")},
+        {cacertfile, filename:join(CertDir, "CA.crt")}
+    ],
+    KraftOpts = #{
+        port => 3030,
+        ssl_opts => SslOpts,
+        app => grisp_manager
+    },
+    KraftRoutes = [
+        {"/grisp-connect/ws",
+            {ws, grisp_manager_device_api}, #{}, #{type => json_rpc}},
+        {"/grisp-manager/ws",
+            {ws, seawapper_ws},
+            #{app => grisp_manager, jsonrpc_api => grisp_manager_ui_api},
+            #{type => json_rpc}},
+        {"/grisp-manager/downloads/:id",
+            {cowboy, grisp_manager_downloads}, #{}},
+        {"/",
+            kraft_static, #{}}
+    ],
+    kraft:start(KraftOpts, KraftRoutes),
+
+    {ok, Started3} = application:ensure_all_started(grisp_manager),
+    Started1 ++ Started2 ++ Started3.
+
+cleanup_apps(Apps) ->
+    mnesia:delete_table(eresu_user),
+    mnesia:delete_table(grisp_device),
+    mnesia:delete_table(grisp_manager_token),
+    [application:stop(App) || App <- Apps],
+    application:stop(mnesia).
+
+get_cert_dir(DataDir) ->
+    SplitDataDir = filename:split(DataDir),
+    JoinedParentDir = filename:join(lists:droplast(SplitDataDir)),
+    CertDir = filename:join(JoinedParentDir, "certs"),
+    true = filelib:is_dir(CertDir),
+    CertDir.
+
+register_user() ->
+    Hash = erlpass:hash(<<"1234">>),
+    WriteUser = fun() ->
+                        mnesia:write({eresu_user,
+                                      <<"Testuser">>,
+                                      <<"foo">>,
+                                      <<"a@a.a">>,
+                                      erlang:system_time(),
+                                      Hash,
+                                      <<"Max Mustermann">>,
+                                      undefined,
+                                      undefined,
+                                      <<"customer_id">>,
+                                      [<<"subscription_id">>]})
+                end,
+    mnesia:activity(transaction, WriteUser).
