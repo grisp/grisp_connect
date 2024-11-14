f012db63ccd4031b622c5c445316bf445f07dd8f
Sébastien Merle <sebastien.merle@stritzinger.com>
2024-11-14 14:49:43 +0100

    Cleanup JsonRPC codec (#51)
    Cleanup JSON-RPC codec
    
    * Move the error formatting out.
    * pre-process and post-process the message to handle null/undefined
       conversion.
    * Return decoding errors as proper JSON-RPC errors that could be sent
       right away to the peer.
    * Fix doc generation.

--- test/grisp_connect_jsonrpc_SUITE.erl
+++ test/grisp_connect_jsonrpc_SUITE.erl
@@ -6,26 +6,30 @@
 
 -export([positional_parameters/1,
         named_parameters/1,
+        using_existing_atoms/1,
         notification/1,
         invalid_json/1,
         invalid_request/1,
         batch/1,
-        result/1]).
+        result/1,
+        null_values/1]).
 
 all() -> [
     positional_parameters,
     named_parameters,
+    using_existing_atoms,
     notification,
     invalid_json,
     invalid_request,
     batch,
-    result
+    result,
+    null_values
 ].
 
 positional_parameters(_) ->
     Term = {request, <<"subtract">>, [42,23], 1},
     Json = <<"{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"subtract\",\"params\":[42,23]}">>,
-    ?assertMatch({single, Term}, grisp_connect_jsonrpc:decode(Json)),
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
     Json2 = grisp_connect_jsonrpc:encode(Term),
     ?assert(jsonrpc_check([<<"\"id\":1">>,
                            <<"\"method\":\"subtract\"">>,
@@ -34,36 +38,44 @@ positional_parameters(_) ->
 named_parameters(_) ->
     Term = {request, <<"divide">>, #{<<"dividend">> => 42, <<"divisor">> => 2}, 2},
     Json = <<"{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"divide\",\"params\":{\"dividend\":42,\"divisor\":2}}">>,
-    ?assertMatch({single, Term}, grisp_connect_jsonrpc:decode(Json)),
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
     Json2 = grisp_connect_jsonrpc:encode(Term),
     ?assert(jsonrpc_check([<<"\"id\":2">>,
                            <<"\"method\":\"divide\"">>,
                            <<"\"dividend\":42">>,
                            <<"\"divisor\":2">>], Json2)).
 
+using_existing_atoms(_) ->
+    % The ID and method are matching existing atoms, checks they are not atoms
+    Term = {request, <<"notification">>, #{}, <<"request">>},
+    Json = <<"{\"id\":\"request\",\"jsonrpc\":\"2.0\",\"method\":\"notification\",\"params\":{}}">>,
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
+    Json2 = grisp_connect_jsonrpc:encode(Term),
+    ?assert(jsonrpc_check([<<"\"id\":\"request\"">>, <<"\"method\":\"notification\"">>], Json2)).
+
 notification(_) ->
     Term = {notification, <<"update">>, [1,2,3,4,5]},
     Json = <<"{\"jsonrpc\":\"2.0\",\"method\":\"update\",\"params\":[1,2,3,4,5]}">>,
-    ?assertMatch({single, Term}, grisp_connect_jsonrpc:decode(Json)),
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
     Json2 = grisp_connect_jsonrpc:encode(Term),
     ?assert(jsonrpc_check([<<"\"method\":\"update\"">>,
                            <<"\"params\":[1,2,3,4,5]">>], Json2)).
 
 invalid_json(_) ->
-    Term = {internal_error, parse_error, null},
+    Term = {decoding_error, -32700, <<"Parse error">>, undefined, undefined},
     Json = <<"{\"jsonrpc\":\"2.0\",\"method\":\"foobar,\"params\":\"bar\",\"baz]">>,
-    ?assertMatch({single, Term}, grisp_connect_jsonrpc:decode(Json)),
-    JsonError = grisp_connect_jsonrpc:encode(grisp_connect_jsonrpc:format_error(Term)),
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
+    JsonError = grisp_connect_jsonrpc:encode(Term),
     ?assert(jsonrpc_check([<<"\"error\":{">>,
                             <<"\"code\":-32700">>,
                             <<"\"message\":\"Parse error\"">>,
                             <<"\"id\":null">>], JsonError)).
 
 invalid_request(_) ->
-    Term = {internal_error, invalid_request, null},
+    Term = {decoding_error, -32600, <<"Invalid request">>, undefined, undefined},
     Json = <<"{\"jsonrpc\":\"2.0\",\"method\":1,\"params\":\"bar\"}">>,
-    ?assertMatch({single, Term}, grisp_connect_jsonrpc:decode(Json)),
-    JsonError = grisp_connect_jsonrpc:encode(grisp_connect_jsonrpc:format_error(Term)),
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
+    JsonError = grisp_connect_jsonrpc:encode(Term),
     ?assert(jsonrpc_check([<<"\"error\":{">>,
                            <<"\"code\":-32600">>,
                            <<"\"message\":\"Invalid request\"">>,
@@ -71,13 +83,13 @@ invalid_request(_) ->
 
 batch(_) ->
     Term1 = {request, <<"sum">>, [1,2,4], <<"1">>},
-    Term2 = {internal_error, invalid_request, null},
+    Term2 = {decoding_error, -32600, <<"Invalid request">>, undefined, undefined},
     Json = <<"[{\"jsonrpc\":\"2.0\",\"method\":\"sum\",\"params\":[1,2,4],\"id\":\"1\"},{\"foo\":\"boo\"}]">>,
-    ?assertMatch({batch, [Term1,Term2]}, grisp_connect_jsonrpc:decode(Json)),
-    JsonError = grisp_connect_jsonrpc:encode([Term1, grisp_connect_jsonrpc:format_error(Term2)]),
+    ?assertMatch([Term1, Term2], grisp_connect_jsonrpc:decode(Json)),
+    JsonError = grisp_connect_jsonrpc:encode([Term1, Term2]),
     ?assert(jsonrpc_check([<<"\"id\":\"1\"">>,
                            <<"\"method\":\"sum\"">>,
-                           <<"params\":[1,2,4]">>,
+                           <<"\"params\":[1,2,4]">>,
                            <<"\"error\":{">>,
                            <<"\"code\":-32600">>,
                            <<"\"message\":\"Invalid request\"">>,
@@ -86,11 +98,20 @@ batch(_) ->
 result(_) ->
     Term = {result, 7, 45},
     Json = <<"{\"id\":45,\"jsonrpc\":\"2.0\",\"result\":7}">>,
-    ?assertMatch({single, Term}, grisp_connect_jsonrpc:decode(Json)),
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
     Json2 = grisp_connect_jsonrpc:encode(Term),
     ?assert(jsonrpc_check([<<"\"id\":45">>,
                            <<"\"result\":7">>], Json2)).
 
+null_values(_) ->
+    Term = {notification, <<"test_null">>, #{array => [undefined], object => #{foo => undefined}, value => undefined}},
+    Json = <<"{\"jsonrpc\":\"2.0\",\"method\":\"test_null\",\"params\":{\"array\":[null],\"object\":{\"foo\":null},\"value\":null}}">>,
+    ?assertMatch(Term, grisp_connect_jsonrpc:decode(Json)),
+    Json2 = grisp_connect_jsonrpc:encode(Term),
+    ?assert(jsonrpc_check([<<"\"array\":[null]">>,
+                           <<"\"foo\":null">>,
+                           <<"\"value\":null">>],
+                          Json2)).
 
 jsonrpc_check(Elements, JsonString) ->
     Elements2 = [<<"\"jsonrpc\":\"2.0\"">>| Elements],
